07c174cd47e8fb842411d53770455add6c3002fa
PeatroXD <PeatroXD@yandex.ru>
2026-04-05 18:48:36 +0300

    init

---
+++ src/main/java/com/peatroxd/streamcutproject/StreamCutProjectApplication.java
@@ -0,0 +1,13 @@
+package com.peatroxd.streamcutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamCutProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamCutProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peatroxd/streamcutproject/StreamCutProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peatroxd.streamcutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamCutProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
