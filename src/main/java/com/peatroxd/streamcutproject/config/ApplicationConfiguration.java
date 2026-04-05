83e172cc24957879cae8c20a5490803798d13c4d
PeatroXD <PeatroXD@yandex.ru>
2026-04-05 20:18:58 +0300

    TASK-001 add health endpoint

---
+++ src/main/java/com/peatroxd/streamcutproject/config/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package com.peatroxd.streamcutproject.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+}
---
+++ src/main/java/com/peatroxd/streamcutproject/job/JobPackageConfiguration.java
@@ -0,0 +1,7 @@
+package com.peatroxd.streamcutproject.job;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobPackageConfiguration {
+}
