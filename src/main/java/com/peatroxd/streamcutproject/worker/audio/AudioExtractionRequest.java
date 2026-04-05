2778ae12163a05ad8a04ab5a8e7b17c51f67b75e
PeatroXD <PeatroXD@yandex.ru>
2026-04-05 19:58:32 +0300

    revert stray java worker audio layer

--- src/main/java/com/peatroxd/streamcutproject/worker/audio/AudioExtractionRequest.java
+++
@@ -1,9 +0,0 @@
-package com.peatroxd.streamcutproject.worker.audio;
-
-import java.nio.file.Path;
-
-public record AudioExtractionRequest(
-        Path inputVideoPath,
-        Path outputAudioPath
-) {
-}
--- src/main/java/com/peatroxd/streamcutproject/worker/audio/ProcessExecutionResult.java
+++
@@ -1,8 +0,0 @@
-package com.peatroxd.streamcutproject.worker.audio;
-
-public record ProcessExecutionResult(
-        int exitCode,
-        String stdout,
-        String stderr
-) {
-}
